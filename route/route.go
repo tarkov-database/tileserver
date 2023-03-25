20d3c016771406c22f67296f4ec3851d79fcd969
Markus Wiegand <mail@morphy2k.dev>
2023-03-25 15:47:36 +0100

    add cors middlware

--- route/route.go
+++ route/route.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	cntrl "github.com/tarkov-database/tileserver/controller"
+	"github.com/tarkov-database/tileserver/middleware/cors"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -19,15 +20,19 @@ func routes() *httprouter.Router {
 	r := httprouter.New()
 
 	// Index
-	r.GET(prefix, cntrl.IndexGET)
+	r.GET(prefix, middlwares(cntrl.IndexGET))
 	r.Handler("GET", "/", http.RedirectHandler(prefix, http.StatusMovedPermanently))
 
 	// Tileset
-	r.GET(prefix+"/:id", cntrl.TileJSONGET)
-	r.GET(prefix+"/:id/tiles/:z/:x/:y", cntrl.TileGET)
+	r.GET(prefix+"/:id", middlwares(cntrl.TileJSONGET))
+	r.GET(prefix+"/:id/tiles/:z/:x/:y", middlwares(cntrl.TileGET))
 
 	r.RedirectTrailingSlash = true
 	r.HandleOPTIONS = true
 
 	return r
+}
+
+func middlwares(h httprouter.Handle) httprouter.Handle {
+	return cors.Handler(h)
 }
